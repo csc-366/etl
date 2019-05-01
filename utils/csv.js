dd87a2c1196be2030aa25c3d1d8b4303054b3f2b
Maxwell Ray <mray02@calpoly.edu>
2019-05-01 10:45:11 -0700

    addition of response helper, print message on server start

--- utils/csv.js
+++ utils/csv.js
@@ -4,7 +4,7 @@ import fs from 'fs';
 export function parse(filename) {
     const s = fs.readFileSync(`./${filename}`).toString();
     const unmappedData = CSV.parse(s, ',', '"');
-    const headers = unmappedData[0];
+    let headers = unmappedData[0];
     return unmappedData.slice(1,unmappedData.length).map((row) => {
         return row.reduce((acc, cur, idx) => {
             acc[headers[idx]] = cur ? cur : null;
