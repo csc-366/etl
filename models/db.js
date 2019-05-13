eb00dd57ad33fa20a113ba5637fcb8fadd014247
Maxwell Ray <mray02@calpoly.edu>
2019-05-12 19:40:59 -0700

    Addition of template tag regex stuff

--- models/db.js
+++ models/db.js
@@ -1,4 +1,4 @@
-import mysql from 'mysql';
+import mysql from 'mysql2/promise';
 import 'dotenv/config';
 import * as util from 'util';
 
@@ -22,8 +22,52 @@ export const query = util.promisify(pool.query).bind(pool);
 export const format = mysql.format;
 
 export async function ingest(data) {
-    data.forEach(async (observation) => {
-        const observation = await o.ingestObservation(observation);
-        const tags = await t.ingestTags(observation)
-    });
+    data
+        .map(async (observation) => {
+            const connection = await pool.acquireConnection();
+            await connection.query("START TRANSACTION");
+
+            observation = {...observation, connection};
+
+            try {
+                // Returns {observationId}
+                const obs = await o.ingestObservation(observation);
+
+                // Returns [{tagId}]
+                const tags = await t.ingestTags(observation);
+                const newTags = tags.filter(({isNew}) => isNew);
+                const oldTags = tags.filter(({isNew}) => !isNew);
+
+                await t.ingestTagObservations(observation, obs, oldTags);
+                await t.ingestTagDeployments(observation, obs, newTags);
+
+                // Returns [{markId}]
+                const marks = await m.ingestMarks(observation);
+                const newMarks = marks.filter(({isNew}) => isNew);
+                const oldMarks = marks.filter(({isNew}) => !isNew);
+
+                await m.ingestMarkObservations(observation, obs, newMarks);
+                await m.ingestMarkDeployments(observation, obs, oldMarks);
+
+                await o.ingestSeal(observation, obs);
+
+                await o.ingestMeasurement(observation, obs);
+
+                await o.ingestFieldLeaders(observation, obs);
+
+                await o.ingestPupAge(observation, obs);
+
+                await o.ingestPupCount(observation, obs);
+
+                await connection.query("COMMIT");
+                return null;
+            } catch (e) {
+                await connection.query('ROLLBACK');
+                return e.message;
+            } finally {
+                connection.release()
+            }
+
+        })
+        .filter(observation => observation);
 }
