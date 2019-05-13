6b05c343daa4e0c55fc11235937b73161bdb6091
Nathaniel Furbeyre <nmfurbearr@outlook.com>
2019-05-12 21:13:02 -0700

    Ingest tag now returns a list of tagIds. Also added error checking for tag position

--- models/tag.js
+++ models/tag.js
@@ -1,6 +1,7 @@
 import {query, format} from "./db";
 
 export const TAG_COLOR_REGEX = /[WBGPVRYO]/;
+export const VALID_TAG_POS_CHARS = 'RLSouil-_';
 
 export const ingestTagPositions = async (positions) => {
     const insertValues = positions.map(({position, nationalTagPosition}) => [position, nationalTagPosition]);
@@ -19,9 +20,27 @@ export const ingestTags = async ({tags}) => {
     }).join(',');
     if (insertValues.length === 0)
         return;
+
+    insertValues.forEach(tag => {
+        if (!TAG_COLOR_REGEX.test(tag.color)) {
+            throw new Error("Bad color")
+        }
+    });
+
+    insertValues.forEach(tag => {
+       for (let i = 0; i < tag.position.length; i++) {
+          if (!VALID_TAG_POS_CHARS.includes(tag.position[i])) {
+              throw new Error("Bad tag")
+          }
+       }
+    });
+
     try {
         const q = `INSERT INTO Tag (Number, Color, Position) VALUES ${insertValues}`;
         await query(q);
+        return insertValues.map(tag => {
+            return tag.number;
+        });
     } catch (e) {
         console.error(e.message)
     }
@@ -31,8 +50,14 @@ export const ingestTag = async ({number, color, position}) => {
     if (!TAG_COLOR_REGEX.test(color)) {
         throw new Error( "Bad color")
     }
+    for (let i = 0; i < position.length; i++) {
+       if (!VALID_TAG_POS_CHARS.includes(position[i])) {
+           throw new Error("Bad tag")
+       }
+    }
     const q = format("INSERT INTO Tag (Number, Color, Position) VALUES (?,?,?)", [number, color, position]);
-    const result = await query(q)
+    const result = await query(q);
+    return (await connection.query("SELECT LAST_INSERT_ID() as ID"))[0][0].ID;
 };
 
 export const ingestTagDeployments = async (deployments) => {
