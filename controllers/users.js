27afc69accea19268450112c623d7ad5f10fc725
Nathaniel Furbeyre <nmfurbearr@outlook.com>
2019-05-13 20:44:03 -0700

    Added routes, controllers and models stubs for users and sessions

---
+++ controllers/users.js
@@ -0,0 +1,24 @@
+
+export async function register(req, res) {
+
+}
+
+export async function getUser(req,res) {
+
+}
+
+export async function deleteUser(req,res) {
+
+}
+
+export async function updateUser(req,res) {
+
+}
+
+export async function approveUser(req,res) {
+
+}
+
+export const validate = (method) => {
+
+}
---
+++ routes/sessions.js
@@ -0,0 +1,25 @@
+import {Router} from 'express';
+import {
+   validate,
+   login,
+   getSession,
+   logout
+} from '../controllers/sessions';
+
+const sessionsRouter = Router();
+express.baseURL = '/sessions';
+
+/*
+ * TODO: Log user in
+ *  - Generate Session Token
+ *  - Remember, do NOT store secrets in plain text in this file!! Put it in .env and use process.env!!
+ *  - Reject invalid username/password combo
+ *  - Reject unapproved account?
+*/
+
+sessionsRouter.post('/login', validate('login'), login);
+sessionsRouter.get('/:cookie', validate('getSession'), getSession);
+sessionsRouter.delete('/logout', validate('logout'), logout);
+
+
+export default sessionsRouter;
